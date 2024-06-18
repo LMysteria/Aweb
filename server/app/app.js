ecf0be7a60d9c9890d81321cd97aafd1140b34e7
LMysteria <thanhnhanjimmy@gmail.com>
2024-06-18 11:52:10 +0700

    edit to deploy

--- server/app/app.js
+++ server/app/app.js
@@ -2,9 +2,11 @@ const express = require('express');
 const MongoConnect = require('./MongoDB/MongoConnect.js')
 const app = express();
 const PORT = 3001;
-
+const path = require('path');
 
 MongoConnect.run;
+
+app.use(express.static(path,resolve(__dirname, '../../client/build')));
 
 app.get('/api', (req, res)=>{
     res.json({message: "Hello from server!"});
